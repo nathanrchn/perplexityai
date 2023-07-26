5f46e0ca457b654e4c1bb5f4f188efa24f11c70e
Nathan Ranchin <nathan.ranchin@epfl.ch>
2023-07-26 12:19:26 +0200

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from Perplexity import Perplexity
-
-perplexity = Perplexity()
-
-while True:
-    inp = str(input("> "))
-    c = perplexity.search(inp)
-    if c:
-        print(c.json_answer_text["answer"])
