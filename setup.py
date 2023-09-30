e06532de7447f0a2b160b2fee9639f36e2426641
Nathan Ranchin <nathan.ranchin@epfl.ch>
2023-09-30 18:53:50 +0200

    rename to perplexityai to avoid confusions

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="perplexity",
+    name="perplexityai",
     version="0.1",
     description="A python api to use perplexity.ai",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nathanrchn/perplexityai",
-    packages=find_packages()
+    packages=find_packages(),
+    requires=["requests", "websockets"]
 )
