0e4bc3003a5c525f25f7d7358fa20c7409f58eae
Nathan Ranchin <nathan.ranchin@epfl.ch>
2023-10-04 21:52:45 +0200

    add chat utils

--- perplexity/utils.py
+++ perplexity/utils.py
@@ -3,5 +3,9 @@ from typing import Iterable, Dict
 def return_just_next_token(answer: Iterable[Dict]) -> str:
     length = 0
     for partial_answer in answer:
-        yield partial_answer["answer"][length:]
-        length = len(partial_answer["answer"])
+        if "answer" in partial_answer:
+            yield partial_answer["answer"][length:]
+            length = len(partial_answer["answer"])
+        elif "output" in partial_answer:
+            yield partial_answer["output"][length:]
+            length = len(partial_answer["output"])
